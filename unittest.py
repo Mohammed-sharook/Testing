aa8cdc8d441da63dc9bb59d5379c81f9cd8b2f10
Mohammed-sharook <aydensharu@gmail.com>
2021-06-16 12:32:00 +0530

    1st commit 16/wed

--- unittest.py
+++ unittest.py
@@ -1,23 +1,23 @@
 import unittest
-import Arithmetics
+import arithmetics
 
 
 class TestTesting(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(Arithmetics.add(10, 5), 15)
+        self.assertEqual(arithmetics.add(10, 5), 15)
 
     def test_sub(self):
-        self.assertEqual(Arithmetics.sub(10, 5), 5)
+        self.assertEqual(arithmetics.sub(10, 5), 5)
 
     def test_mul(self):
-        self.assertEqual(Arithmetics.mul(10, 5), 50)
+        self.assertEqual(arithmetics.mul(10, 5), 50)
 
     def test_div(self):
-        self.assertEqual(Arithmetics.div(10, 5), 2)
-        self.assertEqual(Arithmetics.div(12, 4), 3)
+        self.assertEqual(arithmetics.div(10, 5), 2)
+        self.assertEqual(arithmetics.div(12, 4), 3)
         with self.assertRaises(ValueError):
-            Arithmetics.div(10, 0)
+            arithmetics.div(10, 0)
 
 
 if __name__ == "__main__":
